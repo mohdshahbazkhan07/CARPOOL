dbabb4fcab45bb3f6ecc4a55b7adef0a26faa407
mohdshahbazkhan07 <mohdshahbazkhan0707@gmail.com>
2026-01-28 14:41:52 +0530

    Booking.java

--- untitled/src/Booking.java
+++ untitled/src/Booking.java
@@ -1,26 +1,57 @@
 public class Booking {
-    int Booking_id;
-    int Ride_id;
-    int user_id;
-    int seats_booked;
-    double total_fare;
+    private int booking_id;
+    private int ride_id;
+    private int user_id;
+    private int numberOfSeats;
+    private long totalFare;
+    private String bookingStatus;
 
-    public Booking(int booking_id, int ride_id, int user_id, int seats_booked, double total_fare) {
-        Booking_id = booking_id;
-        Ride_id = ride_id;
+    public Booking(int booking_id, int ride_id, int user_id, int numberOfSeats, long totalFare) {
+        this.booking_id = booking_id;
+        this.ride_id = ride_id;
         this.user_id = user_id;
-        this.seats_booked = seats_booked;
-        this.total_fare = total_fare;
+        this.numberOfSeats = numberOfSeats;
+        this.totalFare = totalFare;
+        this.bookingStatus = "CONFIRMED";
+    }
+
+    // Getters
+    public int getBooking_id() {
+        return booking_id;
+    }
+
+    public int getRide_id() {
+        return ride_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public long getTotalFare() {
+        return totalFare;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    // Cancel booking
+    public void cancelBooking() {
+        this.bookingStatus = "CANCELLED";
     }
 
     @Override
     public String toString() {
-        return "Booking{" +
-                "Booking_id=" + Booking_id +
-                ", Ride_id=" + Ride_id +
-                ", user_id=" + user_id +
-                ", seats_booked=" + seats_booked +
-                ", total_fare=" + total_fare +
-                '}';
+        return "Booking ID : " + booking_id +
+                "\nRide ID : " + ride_id +
+                "\nUser ID : " + user_id +
+                "\nNumber of Seats : " + numberOfSeats +
+                "\nTotal Fare : " + totalFare +
+                "\nStatus : " + bookingStatus;
     }
 }
